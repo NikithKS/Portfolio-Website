31bbeceb64770443b8cf05d012105346ed997d1b
NikithKS <nikithks007@gmail.com>
2020-08-25 15:45:38 +0530

    Added Intersection Observer for animation

--- scripts.js
+++ scripts.js
@@ -1,1 +1,25 @@
 // scripts.js
+
+document.addEventListener("DOMContentLoaded", ready);
+
+
+function ready(){
+    const about = document.getElementById('about-box');
+    let conditions = {
+        threshold: 0.7
+    }
+
+    let trigger = (entries, observer) => {
+        entries.forEach(entry => {
+            if(entry.isIntersecting){
+                about.classList.add('visible');
+                console.log("HERE");
+            }
+        });
+    }
+    let observer = new IntersectionObserver(trigger, conditions);
+
+
+    observer.observe(about);
+    // IntersectionObserver.observe(trigger, about);
+}
